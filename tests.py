c5d0b2d87fdef169a13003be475fc2f5af275203
Sebastien Boisvert <sebhtml@protonmail.com>
2022-03-01 13:20:32 -0500

    (#5) add second unit test

--- tests.py
+++ tests.py
@@ -21,8 +21,32 @@ class TestConvexAreas(unittest.TestCase):
         #                    - 0
         #   |--|--|--|--|--|
         #   0  1  2  3  4  5
-        self.assertEqual(liquid_calculator.ConvexAreas()([1, 2, 4, 2, 4, 1]), 2)
+        heights = [1, 2, 4, 2, 4, 1]
+        self.assertEqual(liquid_calculator.ConvexAreas()(heights), 2)
     
+    def test_case_2(self):
+        #
+        # input = [5,1,3,4,5]
+        # output = 7
+        #
+        #
+        #   *           *    - 5
+        #                    |
+        #            *       - 4
+        #                    |
+        #         *          - 3
+        #                    |
+        #                    - 2
+        #                    |
+        #      *             - 1
+        #                    |
+        #                    - 0
+        #   |--|--|--|--|--|
+        #   0  1  2  3  4  5
+        heights = [5, 1, 3, 4, 5]
+        self.assertEqual(liquid_calculator.ConvexAreas()(heights), 7)
+
+
 if __name__ == '__main__':
     unittest.main()
 
